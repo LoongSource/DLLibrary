582516316b4d4842a5ec535580db98f34672a138
DL <weilong.chen1@gientech.com>
2025-05-27 17:34:53 +0800

    添加测试数据

---
+++ TCommon/src/main/java/com/dl/tcommon/TestFun.kt
@@ -0,0 +1,12 @@
+package com.dl.tcommon
+
+/**
+ * @Author: DL
+ * @Date: 2025/5/27 17:33
+ * @Description:
+ */
+object TestFun {
+    fun test(): String {
+        return "测试test"
+    }
+}
---
+++ TCommon/src/main/java/com/dl/tcommon/TestJava.java
@@ -0,0 +1,12 @@
+package com.dl.tcommon;
+
+/**
+ * @Author: DL
+ * @Date: 2025/5/27 17:34
+ * @Description:
+ */
+public class TestJava {
+    public static String test() {
+        return "result java string";
+    }
+}
