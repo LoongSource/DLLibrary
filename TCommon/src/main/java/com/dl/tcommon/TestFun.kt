0901dbaa17a21811fff7aee8eb2a9404814bae55
DL <weilong.chen1@gientech.com>
2025-05-28 10:58:16 +0800

    删除多余的依赖

--- TCommon/src/main/java/com/dl/tcommon/TestFun.kt
+++ TCommon/src/main/java/com/dl/tcommon/TestFun.kt
@@ -1,12 +1,16 @@
 package com.dl.tcommon
 
+import android.content.Context
+import android.widget.Toast
+
 /**
  * @Author: DL
  * @Date: 2025/5/27 17:33
  * @Description:
  */
 object TestFun {
-    fun test(): String {
+    fun test(context: Context): String {
+        Toast.makeText(context,"调用Kotlin代码成功",Toast.LENGTH_SHORT).show()
         return "测试test"
     }
 }
--- TCommon/src/main/java/com/dl/tcommon/TestJava.java
+++ TCommon/src/main/java/com/dl/tcommon/TestJava.java
@@ -1,12 +1,16 @@
 package com.dl.tcommon;
 
+import android.content.Context;
+import android.widget.Toast;
+
 /**
  * @Author: DL
  * @Date: 2025/5/27 17:34
  * @Description:
  */
 public class TestJava {
-    public static String test() {
+    public static String test(Context context) {
+        Toast.makeText(context, "调用Java代码成功", Toast.LENGTH_SHORT).show();
         return "result java string";
     }
 }
